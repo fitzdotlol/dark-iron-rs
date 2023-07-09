56bc2139c74e4661dfc58817927782b308a56f60
Fitz <me@fitz.lol>
2023-07-09 07:36:48 -0700

    crappy console commands

--- src/console.rs
+++ src/console.rs
@@ -2,6 +2,12 @@
 
 use std::ffi::{c_char, CString};
 
+macro_rules! hook_fn {
+    ($type:ty, $address:expr) => {
+        unsafe { std::mem::transmute::<u32, $type>($address) }
+    };
+}
+
 #[repr(u32)]
 pub enum CommandCategory {
     Debug = 0x0,
@@ -28,10 +34,36 @@ pub enum ConsoleColor {
     Background = 0x8,
 }
 
+pub type ConsoleCommandHandler =
+    extern "fastcall" fn(cmd: *const c_char, args: *const c_char) -> u32;
+
+type def_ConsoleCommandRegister = extern "fastcall" fn(
+    cmd: *const c_char,
+    handler: ConsoleCommandHandler,
+    category: CommandCategory,
+    help: *const c_char,
+) -> u32;
+
+// TODO: think of a clever way to wrap the handler so we can use rust types there
+// FIXME: WoW seems to expect static strings here. I'm sure there's a not-stupid solution,
+//        but you only register a command once, so we're just gonna leak memory here for now...
+pub fn console_command_register(
+    cmd: &str,
+    handler: ConsoleCommandHandler,
+    category: CommandCategory,
+    help: &str,
+) -> u32 {
+    let func = hook_fn!(def_ConsoleCommandRegister, 0x0063F9E0);
+    let c_cmd = CString::new(cmd).unwrap();
+    let c_help = CString::new(help).unwrap();
+
+    return func(c_cmd.into_raw(), handler, category, c_help.into_raw());
+}
+
 type def_ConsoleWriteRaw = extern "fastcall" fn(*const c_char, ConsoleColor);
 
 pub fn console_write(text: &str, color: ConsoleColor) {
-    let func = unsafe { std::mem::transmute::<u32, def_ConsoleWriteRaw>(0x0063CB50) };
+    let func = hook_fn!(def_ConsoleWriteRaw, 0x0063CB50);
     let str = CString::new(text).unwrap();
 
     func(str.as_ptr(), color);
--- src/lib.rs
+++ src/lib.rs
@@ -6,31 +6,47 @@ mod console;
 use once_cell::sync::Lazy;
 use retour::GenericDetour;
 
-use std::os::raw::c_void;
+use std::{ffi::c_char, os::raw::c_void};
 
 use windows::Win32::{
     Foundation::{BOOL, HANDLE},
     System::SystemServices::DLL_PROCESS_ATTACH,
 };
 
-type def_sub_46B840 = unsafe extern "thiscall" fn(u32) -> u32;
-
-static hook_sub_46B840: Lazy<GenericDetour<def_sub_46B840>> = Lazy::new(|| {
-    return unsafe {
-        GenericDetour::new(
-            std::mem::transmute::<u32, def_sub_46B840>(0x0046B840),
-            sub_46B840,
-        )
-        .unwrap()
+macro_rules! ptr {
+    ($address:expr, $type:ty) => {
+        *($address as *mut $type)
     };
-});
+}
+
+extern "fastcall" fn cmd_test(_cmd: *const c_char, _args: *const c_char) -> u32 {
+    console::console_write("this is only a test", console::ConsoleColor::Error);
+    return 0;
+}
 
 fn init_extension() {
-    // enable console
-    unsafe { *std::mem::transmute::<u32, *mut u32>(0xC4EC20) = 1 };
+    unsafe {
+        // enable console
+        ptr!(0x00C4EC20, u32) = 1;
+
+        // Fix InvalidPtrCheck for callbacks outside of .text section
+        ptr!(0x00884800, u32) = 0x00000001;
+        ptr!(0x00884C00, u32) = 0x7FFFFFFF;
+    }
 
     console::console_write("wow112_extension loaded!", console::ConsoleColor::Warning);
+    console::console_command_register("test", cmd_test, console::CommandCategory::Debug, "uhhh");
 }
+
+type def_sub_46B840 = unsafe extern "thiscall" fn(u32) -> u32;
+
+static hook_sub_46B840: Lazy<GenericDetour<def_sub_46B840>> = Lazy::new(|| unsafe {
+    GenericDetour::new(
+        std::mem::transmute::<u32, def_sub_46B840>(0x0046B840),
+        sub_46B840,
+    )
+    .unwrap()
+});
 
 unsafe extern "thiscall" fn sub_46B840(a1: u32) -> u32 {
     init_extension();
